e9e66c2266cde31e06885b23fdce76a922fbdeeb
1rosehip <1rosehip@users.noreply.github.com>
2018-06-18 17:34:00 +0300

    Create remove-the-duplicate-character-in-a-string.js

---
+++ remove-the-duplicate-character-in-a-string.js
@@ -0,0 +1,37 @@
+/**
+ * remove the duplicate characters in a string without using any additional buffer
+ * @param {string} str
+ * @return {string} formatted string
+ * Time complexity is O(n)
+ * Space complexity is O(1)
+ */
+const removeDuplicates = (str) => {
+  
+  let formatted = '';
+  const aCharCode = 'a'.charCodeAt(0);
+  
+  let mask = 0;
+  
+  for(let i=0; i<str.length; i++){
+    
+    const charCode = str.charCodeAt(i);
+    const charCodeDiff = charCode - aCharCode;
+    const charMapped = 1 << charCodeDiff;
+    const and = mask & charMapped;
+    
+    if(and === 0){
+      formatted += str[i];
+    }
+    
+    mask |= charMapped;
+  }
+  
+  return formatted;
+};
+
+// TEST -------------
+const values = ['ccvhhk', '', 'a', 'abc', 'aaa', 'efefef', 'abcdef'];
+
+values.forEach((str, i) => {
+  console.log(`${str}: `, removeDuplicates(str));
+});
