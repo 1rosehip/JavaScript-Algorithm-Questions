0def4f66b4f3c0e09d8c3e0f56e0af65ec67b832
1rosehip <1rosehip@users.noreply.github.com>
2018-07-04 13:48:34 +0300

    Update btree-1.js

--- btree-1.js
+++ btree-1.js
@@ -31,9 +31,7 @@ class BinaryTree{
     
     while(queue.length > 0){
       
-      const saved = queue[queue.length - 1];
-      
-      queue.pop();
+      const saved = queue.pop();
       
       if(!saved.left){
         saved.left = newNode;
@@ -52,6 +50,110 @@ class BinaryTree{
       }
     }
     
+  }
+  
+  /**
+   * find a node
+   * Time complexity O(n) and space compexity O(n)
+   * @param {*} data
+   * @return {object|null} found node
+   */
+  find(data){
+    
+    if(!this.root) return null;
+    
+    const queue = [this.root];
+    
+    while(queue.length > 0){
+      
+      const temp = queue.pop();
+      
+      if(temp.data === data){
+        return temp;
+      }
+      
+      if(temp.left){
+          queue.unshift(temp.left);
+      }
+      
+      if(temp.right){
+          queue.unshift(temp.right);
+      }
+    }
+    
+    return null;
+  }
+  
+  /**
+   * find a deepest node recursive helper
+   * @param {object} root - subtree root
+   * @param {object} params: max level and current deepest node
+   * @param {number} level
+   */
+  findDeepestNodeHelper(root, params, level){
+    if(root){
+      
+      if(root.left){
+        this.findDeepestNodeHelper(root.left, params, level + 1);
+      }
+      
+      if(level > params.maxLevel){
+        params.maxLevel = level;
+        params.deepestNode = root;
+      }
+      
+      if(root.right){
+        this.findDeepestNodeHelper(root.right, params, level + 1);
+      }
+    }
+  }
+  
+  /**
+   * find a deepest node of the binary tree
+   * Time complexity O(n)
+   * @return {object|null} deepest node
+   */
+  findDeepestNode(){
+    
+    if(!this.root) return null;
+    
+    const result = {
+      maxLevel: -1,
+      deepestNode: null
+    };
+    
+    this.findDeepestNodeHelper(this.root, result, 0);
+    
+    return result;
+  }
+  
+  /**
+   * remove a node from the binary tree
+   * @param {*} data
+   */
+  remove(data){
+    
+    // 1. find a node
+    const found = this.find(data);
+    
+    if(found){
+      
+      // 2. find a the deepes and rightmost leaf
+      let deepest = this.findDeepestNode();
+      
+      if(!deepest){
+        
+        //tree has only root
+        this.root = null;
+      }
+      else{
+        // 3. copy data from the leaf to the found node
+        found.data = deepest.data;
+        
+        // 4. remove the leaf
+        deepest = null;
+      }
+    }
   }
   
   /**
@@ -116,3 +218,20 @@ btree.preorder(btree.root);
 
 console.log('Binary tree postorder traversal:');
 btree.postorder(btree.root);
+
+//test find method ------------
+const values = [10, 20, 40, 50, 30, 100, -1, 0];
+values.forEach(val => {
+  console.log(`Find a ${val}: `, btree.find(val));
+});
+
+
+//test 'find deepest node' method
+console.log('Find deepest node: ', btree.findDeepestNode());
+
+//test 'remove' method
+btree.remove(50);
+console.log('After removing 50: ', JSON.stringify(btree, null, 4));
+
+btree.remove(10);
+console.log('After removing 10: ', JSON.stringify(btree, null, 4));
