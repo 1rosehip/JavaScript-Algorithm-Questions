6db5236f30b6423347f6d1bb2b2a047f83f4059b
1rosehip <1rosehip@users.noreply.github.com>
2018-11-18 18:10:43 +0200

    Update dfs.js

--- dfs.js
+++ dfs.js
@@ -35,6 +35,48 @@ const dfs = (graph, start) => {
 
 	}
 };
+
+/**
+ * BFS
+ * Time complexity: O (V + E)
+ * Space complexity: O(|E|)
+ * @param {Array.<Array>} graph - Adjacency matrix
+ * @param {string} start node
+ */
+const bfs = (graph, start) => {
+
+	const stack = [start];
+	const visited = {};
+	const A_CODE = 65;
+
+	while(stack.length > 0){
+
+		const node = stack.pop();
+
+		if(!visited[node]){
+
+			console.log(node);
+			visited[node] = true;
+	
+			//find first non visited ajecent node and put it to the stack
+			//1. find ajecent nodes
+			const index = node.charCodeAt(0) - A_CODE; // 'A' = 65, 65 - 65 = 0
+			const edges = graph[index];
+	
+			//2. what nodes are non-visited?
+			for(let i=0; i<edges.length; i++){
+	
+				const edge = edges[i];
+				const letter = String.fromCharCode(i + A_CODE);
+				if(!visited[letter] && edge){
+					stack.push(letter);
+				}
+			}
+		}
+
+	}
+};
+
 //              A  B  C  D  E  F            
 const graph = [[1, 1, 0, 0, 1, 0],  //A
                [1, 0, 1, 0, 1, 0],  //B
@@ -44,7 +86,7 @@ const graph = [[1, 1, 0, 0, 1, 0],  //A
 			   [0, 0, 0, 1, 0, 0]]; //F
 
 			   
-dfs(graph, 'A'); // A -> B -> C -> D -> E -> F
+bfs(graph, 'A'); // A -> B -> C -> D -> E -> F
 /*
 A -> B
 A -> E
